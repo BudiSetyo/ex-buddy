48a7c9de1d035b165e81ee448eba7b91e068d899
BudiSetyo <budisetyo176@gmail.com>
2021-04-07 14:25:28 +0700

    update

---
+++ src/models/subClass.js
@@ -0,0 +1,16 @@
+const connect = require('../database/connection');
+
+const getSubClass = (id, course) => {
+  return new Promise((resolve, reject) => {
+    const queryString = `SELECT sub_class_name AS SubClass, score AS Score, day AS Day, start_time AS StartTime, end_time AS EndTime FROM users_progress JOIN sub_class ON users_progress.id_sub_class = sub_class.id JOIN class ON users_progress.id_class = class.id_class JOIN my_class ON users_progress.id_user_class = my_class.id WHERE my_class.id_user=? AND my_class.id_class=?`;
+
+    connect.query(queryString, [id, course], (err, result) => {
+      if (err) reject(err);
+      resolve(result);
+    });
+  });
+};
+
+module.exports = {
+  getSubClass,
+};
