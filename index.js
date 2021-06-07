ca531ca51c1d35fa086369bb1cbe4a583b828d86
BudiSetyo <budisetyo176@gmail.com>
2021-06-07 11:17:39 +0700

    update db sql

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 require('dotenv').config();
-// import express from 'express';
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
 const Router = require('./src/routers/router');
 
-//init express app
 const app = express();
 
-// config express
-// listen
 app.listen(process.env.PORT, () => {
   console.log('Server Running at Port', process.env.PORT);
 });
 
-// middleware untuk parsing body
-// content-type = application/json
-// body json
 const jsonParser = express.json();
-// content-type = application/x-www-form-urlencoded
-// body x-www-form-urlencoded
 const urlEncodedParser = express.urlencoded();
 
 app.use(logger('dev'));
--- sql/dbSql.sql
+++ sql/dbSql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 19 Apr 2021 pada 08.14
+-- Waktu pembuatan: 07 Jun 2021 pada 06.15
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 8.0.0
 
@@ -52,11 +52,12 @@ INSERT INTO `categories` (`id`, `category_name`) VALUES
 
 CREATE TABLE `class` (
   `id` int(11) NOT NULL,
+  `id_fasilitator` int(11) NOT NULL,
   `class_name` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `day` int(11) NOT NULL,
-  `start_time` time NOT NULL,
-  `end_time` time NOT NULL,
+  `start_time` time DEFAULT NULL,
+  `end_time` time DEFAULT NULL,
   `category` int(11) NOT NULL,
   `level` int(11) NOT NULL,
   `pricing` int(11) NOT NULL,
@@ -67,19 +68,23 @@ CREATE TABLE `class` (
 -- Dumping data untuk tabel `class`
 --
 
-INSERT INTO `class` (`id`, `class_name`, `description`, `day`, `start_time`, `end_time`, `category`, `level`, `pricing`, `class_img`) VALUES
-(1, 'Know more Javascript', 'JavaScript is a cross-platform, object-oriented scripting language used to make webpages interactive (e.g., having complex animations, clickable buttons, popup menus, etc.).', 1, '08:00:00', '11:00:00', 1, 1, 0, ''),
-(2, 'HTML and CSS to code', 'HTML (the Hypertext Markup Language) and CSS (Cascading Style Sheets) are two of the core technologies for building Web pages.', 2, '08:00:00', '11:00:00', 1, 2, 10, ''),
-(3, 'Indonesian war history', 'The military history of Indonesia includes the military history of the modern nation of Republic of Indonesia, as well as the military history of the states which preceded and formed it. It encompassed a kaleidoscope of conflicts spanning over a millennia.', 3, '08:00:00', '11:00:00', 2, 3, 50, ''),
-(4, 'Buddhism and Modern Psychology', 'The Dalai Lama has said that Buddhism and science are deeply compatible and has encouraged Western scholars to critically examine both the meditative practice and Buddhist ideas about the human mind. A number of scientists and philosophers have taken up this challenge. There have been brain scans of meditators and philosophical examinations of Buddhist doctrines. There have even been discussions of Darwin and the Buddha: Do early Buddhist descriptions of the mind, and of the human condition, make particular sense in light of evolutionary psychology? \r\n\r\nThis course will examine how Buddhism is faring under this scrutiny. Are neuroscientists starting to understand how meditation “works”? Would such an understanding validate meditation—or might physical explanations of meditation undermine the spiritual significance attributed to it? And how are some of the basic Buddhist claims about the human mind holding up? We’ll pay special attention to some highly counterintuitive doctrines: that the self doesn’t exist, and that much of perceived reality is in some sense illusory. Do these claims, radical as they sound, make a certain kind of sense in light of modern psychology? And what are the implications of all this for how we should live our lives? Can meditation make us not just happier, but better people?', 4, '08:00:00', '11:00:00', 6, 1, 0, ''),
-(5, 'Financial markets', 'Financial Market refers to a marketplace, where creation and trading of financial assets, such as shares, debentures, bonds, derivatives, currencies, etc.', 5, '08:00:00', '11:00:00', 5, 2, 10, ''),
-(6, 'Corporate finance', 'Corporate finance is the division of finance that deals with how corporations deal with funding sources, capital structuring, and investment decisions.', 6, '08:00:00', '11:00:00', 5, 3, 50, ''),
-(7, 'Algorithm specialization', 'This specialization is an introduction to algorithms for learners with at least a little programming experience.', 1, '11:00:00', '13:00:00', 3, 3, 50, ''),
-(8, 'Business and Financial Modeling', 'Financial modeling is a representation in numbers of a company\'s operations in the past, present, and the forecasted future.', 2, '11:00:00', '13:00:00', 1, 1, 0, ''),
-(9, 'Marketing in a Digital World', 'Marketing in a Digital World is one of the most popular courses on Coursera with over 250,000 Learners and is rated by Class Central as one of the Top 50 MOOCs of All Time (https://www.class-central.com/report/top-moocs/). The course was initially launched in 2015, and has been updated in April 2020.', 3, '11:00:00', '13:00:00', 1, 2, 10, ''),
-(10, 'Social Psychology', 'Social psychology is the scientific study of how people\'s thoughts, feelings, beliefs, intentions and goals are constructed within a social context by the actual or imagined interactions with others.', 4, '11:00:00', '13:00:00', 6, 3, 50, ''),
-(16, 'Front-end fundamentals', 'Learn the fundamentals of front end...', 5, '11:00:00', '13:00:00', 1, 1, 0, ''),
-(25, 'state management redux', 'redux is state management......', 4, '08:00:00', '10:00:00', 1, 2, 20, '');
+INSERT INTO `class` (`id`, `id_fasilitator`, `class_name`, `description`, `day`, `start_time`, `end_time`, `category`, `level`, `pricing`, `class_img`) VALUES
+(1, 35, 'Know more Javascript', 'JavaScript is a cross-platform, object-oriented scripting language used to make webpages interactive (e.g., having complex animations, clickable buttons, popup menus, etc.).', 1, '08:00:00', '11:00:00', 1, 1, 0, ''),
+(2, 35, 'HTML and CSS to code', 'HTML (the Hypertext Markup Language) and CSS (Cascading Style Sheets) are two of the core technologies for building Web pages.', 2, '08:00:00', '11:00:00', 1, 2, 10, ''),
+(3, 0, 'Indonesian war history', 'The military history of Indonesia includes the military history of the modern nation of Republic of Indonesia, as well as the military history of the states which preceded and formed it. It encompassed a kaleidoscope of conflicts spanning over a millennia.', 3, '08:00:00', '11:00:00', 2, 3, 50, ''),
+(4, 0, 'Buddhism and Modern Psychology', 'The Dalai Lama has said that Buddhism and science are deeply compatible and has encouraged Western scholars to critically examine both the meditative practice and Buddhist ideas about the human mind. A number of scientists and philosophers have taken up this challenge. There have been brain scans of meditators and philosophical examinations of Buddhist doctrines. There have even been discussions of Darwin and the Buddha: Do early Buddhist descriptions of the mind, and of the human condition, make particular sense in light of evolutionary psychology? \r\n\r\nThis course will examine how Buddhism is faring under this scrutiny. Are neuroscientists starting to understand how meditation “works”? Would such an understanding validate meditation—or might physical explanations of meditation undermine the spiritual significance attributed to it? And how are some of the basic Buddhist claims about the human mind holding up? We’ll pay special attention to some highly counterintuitive doctrines: that the self doesn’t exist, and that much of perceived reality is in some sense illusory. Do these claims, radical as they sound, make a certain kind of sense in light of modern psychology? And what are the implications of all this for how we should live our lives? Can meditation make us not just happier, but better people?', 4, '08:00:00', '11:00:00', 6, 1, 0, ''),
+(5, 0, 'Financial markets', 'Financial Market refers to a marketplace, where creation and trading of financial assets, such as shares, debentures, bonds, derivatives, currencies, etc.', 5, '08:00:00', '11:00:00', 5, 2, 10, ''),
+(6, 0, 'Corporate finance', 'Corporate finance is the division of finance that deals with how corporations deal with funding sources, capital structuring, and investment decisions.', 6, '08:00:00', '11:00:00', 5, 3, 50, ''),
+(7, 0, 'Algorithm specialization', 'This specialization is an introduction to algorithms for learners with at least a little programming experience.', 1, '11:00:00', '13:00:00', 3, 3, 50, ''),
+(8, 35, 'Business and Financial Modeling', 'Financial modeling is a representation in numbers of a company\'s operations in the past, present, and the forecasted future.', 2, '11:00:00', '13:00:00', 1, 1, 0, ''),
+(9, 35, 'Marketing in a Digital World', 'Marketing in a Digital World is one of the most popular courses on Coursera with over 250,000 Learners and is rated by Class Central as one of the Top 50 MOOCs of All Time (https://www.class-central.com/report/top-moocs/). The course was initially launched in 2015, and has been updated in April 2020.', 3, '11:00:00', '13:00:00', 1, 2, 10, ''),
+(10, 0, 'Social Psychology', 'Social psychology is the scientific study of how people\'s thoughts, feelings, beliefs, intentions and goals are constructed within a social context by the actual or imagined interactions with others.', 4, '11:00:00', '13:00:00', 6, 3, 50, ''),
+(16, 35, 'Front-end fundamentals', 'Learn the fundamentals of front end...', 5, '11:00:00', '13:00:00', 1, 1, 0, ''),
+(25, 35, 'State management redux', 'Redux is an open-source JavaScript library for managing application state. It is most commonly used with libraries such as React or Angular for building user interfaces.', 0, '08:00:00', '10:00:00', 1, 2, 20, ''),
+(28, 0, 'History of Europe', 'The history of Europe concerns itself with the discovery and collection, the study, organization and presentation and the interpretation of past events and affairs of the people of Europe since the beginning of written records.', 0, '11:00:00', '13:00:00', 2, 2, 10, ''),
+(30, 35, 'Vue js', 'vue js is Ui framework', 3, '11:00:00', '13:00:00', 1, 3, 50, ''),
+(31, 35, 'Trigonometri', 'Trygonometri is', 1, NULL, NULL, 3, 1, 10, '/images/1622077594284-image.jpg'),
+(32, 35, 'redux', 'description\nasdasd', 1, NULL, NULL, 1, 1, 10, '/images/1622106386617-image.jpg');
 
 -- --------------------------------------------------------
 
@@ -98,7 +103,7 @@ CREATE TABLE `levels` (
 
 INSERT INTO `levels` (`id`, `level_name`) VALUES
 (1, 'Beginner'),
-(2, 'Intermediet'),
+(2, 'Intermediate'),
 (3, 'Advance');
 
 -- --------------------------------------------------------
@@ -128,7 +133,11 @@ INSERT INTO `my_class` (`id`, `id_user`, `id_class`) VALUES
 (11, 36, 16),
 (13, 61, 2),
 (14, 61, 3),
-(15, 34, 7);
+(15, 34, 7),
+(16, 77, 3),
+(17, 34, 2),
+(18, 34, 3),
+(19, 34, 5);
 
 -- --------------------------------------------------------
 
@@ -198,23 +207,21 @@ CREATE TABLE `users` (
   `user_name` varchar(255) NOT NULL,
   `phone_number` varchar(255) NOT NULL,
   `gender` enum('L','P') NOT NULL,
-  `role` varchar(255) NOT NULL,
-  `profile_img` varchar(255) NOT NULL
+  `role` int(11) NOT NULL DEFAULT 1,
+  `profile_img` varchar(255) NOT NULL,
+  `otp` varchar(4) NOT NULL,
+  `otp_expired` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `full_name`, `email`, `password`, `user_name`, `phone_number`, `gender`, `role`, `profile_img`) VALUES
-(34, '', 'betabet@gmail.com', '$2b$10$rP5Qy8i44457qpo0IFrtieiIw5l0657qLysM40exTzauNm4l.TlT.', 'dewakipas', '', 'L', '1', ''),
-(35, 'bambang setyo budi', 'buddys@gmail.com', '$2b$10$iyBZCLmyvld7WDfAWHHmsuFxEoVCPz1hRPH9iRslr2Y2IZSaHJs9O', 'setyo', '08981928912', 'L', '2', ''),
-(36, '', 'meyla@gmail.com', '$2b$10$5Fgj4apjENjIEktAx11wyuFMH39I0OgTWrqn5/hFMuGPw31Lvn6ei', 'rauf', '', 'L', '', ''),
-(37, '', 'roboto@gmail.com', '$2b$10$iZEHV8qHVSSplIm40Ezfy.nv.b9pObwOj.o/wd3SmTY8lpah52kpG', 'roboto', '', 'L', '', ''),
-(38, '', 'gimangkuy@gmail.com', '$2b$10$pa3hBCNXEyrJjFFERvY/AeuOojXJt.L0x0vfGYOYxXvkw6HBOBlHm', 'gimangkuy', '', 'L', '', ''),
-(60, '', 'meylalal@gmail.com', '$2b$10$7z8C/H.A9JYp2OrkrwNWduPVNvX6KkBFOFtB2mM1u/Aed/RYxIpZu', 'raufa', '', 'L', '', ''),
-(61, '', 'buddysety@gmail.com', '$2b$10$t8HXMs.PSEfeuMKjAXHvyumXrgOx/0xABwOCVWHa2UgJLuung17Ma', 'budiSetyo', '', 'L', '', ''),
-(62, '', 'blibli@coba.com', '$2b$10$sDRLipui2KuwF48RcjTuSuCj51nb9jGwaNhMOe2Ecy0JU/hmbE3ie', 'blibli', '', 'L', '', '');
+INSERT INTO `users` (`id`, `full_name`, `email`, `password`, `user_name`, `phone_number`, `gender`, `role`, `profile_img`, `otp`, `otp_expired`) VALUES
+(34, '', 'betabet@gmail.com', '$2b$10$SBS56FhksBM0o0B2v9h91uucK1nnIJCdYsn67rgI2QpyZaw/GIera', 'dewakipas', '085238956085', 'L', 1, '/images/1622776254212-image.jpg', '', 1622772371581),
+(35, 'bambang setyo budi', 'buddys@gmail.com', '$2b$10$PiUeakV43xiXdLze79UAdecAmqxsSJpSg/Q/ipVu2d/5q9mSQW1Tu', 'setyo', '082456786682', 'L', 2, '/images/1622767654011-image.jpg', '', 1622081697972),
+(77, '', 'bamboo@gmail.com', '$2b$10$lfnF8hRnkh3Mwg6fNx.b1OmVp4VLZqlyJjRUstSGgfO06eCR5z.l2', 'bamboo', '', 'L', 1, '', '', 1621102799961),
+(78, '', 'User@gmail.com', '$2b$10$.qh/E0E0e9cGs9nI2ikcVuZYnnOUt8J6tx4XWUXlbwLqsUczicoQO', 'User', '', 'L', 1, '', '', 1622753871655);
 
 -- --------------------------------------------------------
 
@@ -317,7 +324,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT untuk tabel `class`
 --
 ALTER TABLE `class`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT untuk tabel `levels`
@@ -329,7 +336,7 @@ ALTER TABLE `levels`
 -- AUTO_INCREMENT untuk tabel `my_class`
 --
 ALTER TABLE `my_class`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT untuk tabel `roles`
@@ -347,7 +354,7 @@ ALTER TABLE `sub_class`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- AUTO_INCREMENT untuk tabel `users_progress`
